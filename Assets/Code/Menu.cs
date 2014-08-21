d82a5dafb457872e7a8e24fd18f7a4ad271bc3ba
Sean Loper <Grimmdev@gmail.com>
2014-08-21 18:20:28 -0400

    Initial Release, HAVE FUN!

---
+++ Assets/Code/Menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// ignore this script, just to load the other scenes
+public class Menu : MonoBehaviour {
+
+	public GUISkin gui;
+	
+	void OnGUI()
+	{
+		GUI.skin = gui;
+		if(GUILayout.Button("Demo1")){
+			Application.LoadLevel(0);
+		}
+		if(GUILayout.Button("Demo2")) {
+			Application.LoadLevel(1);
+		}
+		if(GUILayout.Button("Demo3")){
+			Application.LoadLevel(2);
+		}
+	}
+}
---
+++ Assets/Code/TestAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnimation : MonoBehaviour {
+
+	// we use the animator for this
+	private Animator thisAnimator;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		thisAnimator = this.GetComponent<Animator> ();
+		// make sure we have the animator..
+		if (thisAnimator != null)
+		{
+			// since the game loaded, do a fake loading thing!
+			StartCoroutine (FakeLoading (Mathf.Infinity));
+		}
+	}
+
+	// pretends to load something.. by using a simple counter
+	IEnumerator FakeLoading(float time){
+		// this begins to play the animation
+		thisAnimator.SetBool ("Play", true);
+		// this is our counter or timer
+		yield return new WaitForSeconds(time);
+		// now we stop the animation, because now is when it's loaded stuff!
+		thisAnimator.SetBool ("Play", false);
+	}
+}
